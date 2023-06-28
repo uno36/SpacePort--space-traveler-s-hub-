ac9ddaabebdab81d07d0ef673e81a5ba60079cb8
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-06-28 13:03:45 +0200

    Add reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import rocketsReducer from './reducers/rockets/rocketsReducers';
 import missionsReducer from './reducers/missions/missionsReducers';
 
 const rootReducer = combineReducers({
+  rockets: rocketsReducer,
   missions: missionsReducer,
 });
 
